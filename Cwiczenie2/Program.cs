41eeddeaad162519b5aeb8b3652a8fcc383895df
powermtest <powermtest@gmail.com>
2018-08-03 12:48:47 +0200

    zaczynamy drugie cwiczenie

---
+++ Cwiczenie2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cwiczenie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads a text file and displays the longest word in the file.
+            
+            
+        }
+    }
+}
