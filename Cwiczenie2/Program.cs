117d13ea490b083b7256486694db71918ebf7a5d
powermtest <powermtest@gmail.com>
2018-08-03 13:54:10 +0200

    Ćwiczenie skończone
    Nie chce mi się szukać czy były inne, równie długie słowa... To jest pierwsze najdłuższe.

--- Cwiczenie2/Program.cs
+++ Cwiczenie2/Program.cs
@@ -12,8 +12,36 @@ namespace Cwiczenie2
         static void Main(string[] args)
         {
             //Write a program that reads a text file and displays the longest word in the file.
-            
-            
+            var path = @"C:\temp\12\test.txt";
+
+            var zawartoscPliku = File.ReadAllText(path);
+            var dlugoscSlowa = 0;
+            //var licznik = 0;
+            var maxDlugoscSlowa = 0;
+            var slowo = new StringBuilder();
+            var najdluzszeSlowo = string.Empty;
+            foreach (var literka in zawartoscPliku)
+            {
+                if (char.IsLetter(literka) && !char.IsWhiteSpace(literka))
+                {
+                    slowo.Append(literka);
+                    dlugoscSlowa++;
+                    continue;
+                }
+                
+                if (dlugoscSlowa > maxDlugoscSlowa)
+                {
+                    maxDlugoscSlowa = dlugoscSlowa;
+                    najdluzszeSlowo = slowo.ToString();
+
+                }
+                dlugoscSlowa = 0;
+                slowo.Clear();
+
+            }
+            Console.WriteLine("Pierwsze, najdłuższe znalezione słowo to: >>" + najdluzszeSlowo + "<<, składa się ono z "+ maxDlugoscSlowa+ " znaków.");
+
+
         }
     }
 }
